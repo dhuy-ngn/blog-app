57c421e41fa5a66b257856386ccc14cb068be1ce
HuyND <huynd@inter-k.com>
2024-08-22 14:49:34 +0700

    add (post): post item page

---
+++ app/routes/posts.$postId.tsx
@@ -0,0 +1,26 @@
+import { json, type LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import AppDataSource from '~/db.server';
+import { PostEntity } from '~/db/entities/post.entity';
+
+export const loader = async ({ params: {postId} } : LoaderFunctionArgs) => {
+  const postRepository = AppDataSource.getRepository(PostEntity);
+  const post = await postRepository.findOneBy({ id: Number(postId) });
+
+  if (!post) {
+    throw new Response("Post not found", { status: 404 });
+  }
+
+  return json(post)
+};
+
+export default function Post() {
+  const post = useLoaderData<PostEntity>();
+
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+    </>
+  )
+}

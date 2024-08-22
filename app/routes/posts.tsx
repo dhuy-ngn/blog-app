c46484322670b11bbab21aa172711d120a7b9332
HuyND <huynd@inter-k.com>
2024-08-22 17:31:00 +0700

    add (post): search post title by string

--- app/routes/posts.tsx
+++ app/routes/posts.tsx
@@ -1,12 +1,15 @@
+import type { LoaderFunctionArgs } from '@remix-run/node';
 import { Form, json, Link, useLoaderData } from '@remix-run/react';
+import { Like } from 'typeorm';
 
 import AppDataSource from '~/db.server';
 import { PostEntity } from '~/db/entities/post.entity';
 
-export const loader = async () => {
-  const postRepository = AppDataSource.getRepository(PostEntity);
-  const posts = await postRepository.find();
-
+export const loader = async ({request}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const posts = await findPostWithTitleContaining(String(q))
+  
   return json(posts)
 };
 
@@ -19,11 +22,33 @@ export const action = async () => {
   return json(emptyPost);
 }
 
+async function findPostWithTitleContaining(searchString: string) {
+  console.log({searchString})
+  const postRepository = AppDataSource.getRepository(PostEntity);
+
+  if (!searchString) {
+    return postRepository.find();
+  }
+
+  const formattedSearchString = searchString.trim();
+  return await postRepository.find({
+    where: {
+      title: Like(`%${formattedSearchString}%`)
+    }
+  });
+}
+
 export default function PostList() {
   const posts = useLoaderData<PostEntity[]>();
 
   return (
     <>
+      <Form role="search">
+        <input
+          name="q"
+          placeholder='Search posts...'
+        />
+      </Form>
       <Form method='post'>
         <button type='submit'>Add Post</button>
       </Form>
